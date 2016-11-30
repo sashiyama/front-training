12bd20783fc2e310b78da8667c69d2dbb6b154f5
Yoshiki.Sashiyama <yoshiki.sashiyama@nijibox.co.jp>
2016-11-30 11:54:16 +0900

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontTrainingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontTrainingApp
+ */
+angular.module('frontTrainingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontTrainingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontTrainingApp
+ */
+angular.module('frontTrainingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
